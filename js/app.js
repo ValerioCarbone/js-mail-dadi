a4c9968b4b28271fc33636ec922c1eca204b4478
ValerioCarbone <valeriocarbone10@gmail.com>
2023-10-09 16:36:40 +0200

    CHANGE- Code optimized

--- js/app.js
+++ js/app.js
@@ -1,9 +1,7 @@
 // # Mail
 // - Creare array con mail utenti
 const mailsArray = ['pincopallino@gmail.com', 'valeriocarbone@gmail.com', 'boolean123@gmail.com' ,'paolorosso@gmail.com', 'tizio123@gmail.com' ,'caio123@gmail.com'];
-console.log(mailsArray);
-console.log(mailsArray.length);
-console.log(mailsArray[0])
+
 // - Chiedere tramite prompt la mail all'utente
 let userMail = prompt('Inserisci la tua mail');
 
@@ -14,15 +12,16 @@ let numberMailsArray = mailsArray.length - 1;
 //      - Creare un ciclo for
 //      - Controllare l'email inserita con ogni mail presente nell'array
 //      - Stampare messaggio in funzione del risultato
+let message = ('Email inserita non corretta')
+
 for (let i = 0 ; i <= numberMailsArray ; i++) {
     if (userMail === mailsArray[i]) {
-        console.log('Email corretta')
+        message = ('Email corretta')
         i = numberMailsArray
     }
-    else {
-        console.log('Email errata')
-    }
 }
+
+console.log(message)
 
 
 
